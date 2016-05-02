842b81e30d06be6f9c0054ad6e4dc621e7c3c3c7
Kristian Ellingsen <Kristian Ellingsen>
2016-05-02 10:39:52 +0200

    blablabla

--- Snake/main.asm
+++ Snake/main.asm
@@ -51,13 +51,15 @@ init:
      ldi rTemp, LOW(RAMEND)
      out SPL, rTemp
 // */
-
+	
+	ldi r22, 0x00
+	ldi r21, 0x01
 	ldi r16, 0xff ; Sätt r16 (rTemp) till 11111111 (255)
 	out DDRB, r16 ; Sätt alla I/O-portar till output? (ettor på allt)
 	out DDRC, r16
 	out DDRD, r16
 
-	; Sätt även joystickar till output?
+	; Sätter joystickar till input!
 	cbi DDRC, PC4 ; 
 	cbi DDRC, PC5 //DDR klar
 
@@ -77,6 +79,15 @@ init:
 	rjmp    loop
 	nop */
 
+	cbi PORTC, PC0
+	cbi PORTC, PC1
+	cbi PORTC, PC2
+	cbi PORTC, PC3
+	cbi PORTD, PD2
+	cbi PORTD, PD3
+	cbi PORTD, PD4
+	cbi PORTD, PD5
+
 loop:
 
 	; Få översta raden att lysa genom bitmanipulering, bst / bld
@@ -85,6 +96,29 @@ loop:
 	ldi YL, LOW(matrix)
 	st Y, r16 ; Y = 11111111
 
+	/*sbi PORTC, PC0
+	rcall Laddarad
+	st Y, r22
+	rcall Laddarad
+	cbi PORTC, PC0
+	st Y, r21
+	sbi PORTC, PC1
+	rcall Laddarad
+	st Y, r22
+	rcall Laddarad
+	cbi PORTC, PC1*/
+	st Y, r16
+	sbi PORTD, PD2
+	rcall Laddarad
+	cbi PORTD, PD2
+	st Y, r22
+	rcall Laddarad
+
+	rjmp	loop
+	nop
+
+
+Laddarad:
 	ld rMellan, Y ; rMellan = Y
 
 	; bst - stores bit b from the Rd (rMellan) to the T flag in SREG (status register)
@@ -118,8 +152,4 @@ loop:
 	out PORTD, rPORTD
 	out PORTB, rPORTB
 
-	; ?
-	sbi PORTC, PC0
-
-	rjmp	loop
-	nop
+	ret
